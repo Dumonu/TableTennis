0492134e838db0c90248441bc20aad2dea8f48a9
Dumonu <jacobcherry36@gmail.com>
2018-03-27 23:09:28 -0500

    Added Client.java. Checks if arguments exist.

---
+++ Client.java
@@ -0,0 +1,12 @@
+import java.net.Socket;
+
+public class Client
+{
+    public static void Main(String[] args)
+    {
+        if(args.length < 2)
+            throw new RuntimeException("Invalid arguments, please add IP and Port");
+
+
+    }
+}
